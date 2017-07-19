1f5514e30dea597ea1b6dbe7008826a8811483e2
Chan <cnaseeb@users.noreply.github.com>
2017-07-19 16:43:41 +0200

    Update main.java

--- main.java
+++ main.java
@@ -203,12 +203,4 @@ public class Main {
             }
         }
 
-class Block {
-    constructor(index, previousHash, timestamp, data, hash) {
-        this.index = index;
-        this.previousHash = previousHash.toString();
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = hash.toString();
-    }
-}
+
