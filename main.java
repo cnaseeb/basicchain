d8dbf3a38b5873f453f66383c825f0753f809874
Chan <cnaseeb@users.noreply.github.com>
2017-07-19 16:42:42 +0200

    Update main.java

--- main.java
+++ main.java
@@ -8,16 +8,6 @@ var http_port = process.env.HTTP_PORT || 3001;
 var p2p_port = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
-class Block {
-    constructor(index, previousHash, timestamp, data, hash) {
-        this.index = index;
-        this.previousHash = previousHash.toString();
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = hash.toString();
-    }
-}
-
 var sockets = [];
 var MessageType = {
     QUERY_LATEST: 0,
